aab3666b11b54d951d118820a70ffc164b8bc3d6
ShobhitVishnoi30 <vishnoishobhit201@gmail.com>
2023-04-27 11:49:08 +0530

    feat:security updates

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,58 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { ConfigService } from '@nestjs/config';
+import * as hpp from 'hpp';
+import helmet from 'helmet';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<string>('PORT') || 3000;
+
+  app.enableCors();
+
+  app.use(hpp());
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          defaultSrc: [
+            "'self'",
+            // "data:",
+            // "blob:",
+            'https://*.cloudflare.com',
+            `http://localhost:${port}/`,
+            // "ws:",
+          ],
+          baseUri: ["'self'"],
+          scriptSrc: [
+            'self',
+            `http://localhost:${port}/`,
+            `http://127.0.0.1:${port}/`,
+            'https://*.cloudflare.com',
+            'https://polyfill.io',
+            // "http:",
+            // "data:",
+          ],
+          styleSrc: ['self', 'https:', 'http:', 'unsafe-inline'],
+          imgSrc: ["'self'", 'data:', 'blob:'],
+          fontSrc: ["'self'", 'https:', 'data:'],
+          childSrc: ["'self'", 'blob:'],
+          styleSrcAttr: ["'self'", 'unsafe-inline', 'http:'],
+          frameSrc: ["'self'"],
+        },
+      },
+    }),
+  );
+
+  // reading url params and data from body to req.params and req.body
+  app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+  app.use(express.json({ limit: '10kb' })); // for bodyParser
+
+  //also use body parser here itself remove other custom middle ware
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
--- src/middleware/hpp.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import * as hpp from 'hpp';
-
-@Injectable()
-export class HppMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    hpp()(req, res, next);
-  }
-}
